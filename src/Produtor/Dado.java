9a9cb84bb86f7a0115fa4ec3d7c01b5dbe2e0e2c
Elivelton <Elivelton@192.168.25.6>
2016-10-07 18:57:44 -0300

    Inclusão arquivos Produtor.

---
+++ src/Produtor/Dado.java
@@ -0,0 +1,13 @@
+package Produtor;
+
+public class Dado {
+    private String nome;
+
+    public Dado(String nomeDado) {
+        this.nome = nomeDado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/Produtor/Main.java
@@ -0,0 +1,23 @@
+package Produtor;
+
+import java.util.Scanner;
+
+public class Main {
+    private static int numeroItens = 3;
+    private static int numeroTransacoes = 4;
+    private static int numeroAcessos = 9;
+    private static Scanner scanner;
+	
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        System.out.println( "Criando transacoes e gravando no banco..." );
+        Produtor produtor = new Produtor(numeroItens, numeroTransacoes, numeroAcessos);
+        produtor.start();
+        System.out.println("Pressione Enter para encerrar a producao!");
+
+        if(scanner.hasNextLine()) {
+            System.out.println("Producao encerrada");
+            produtor.setFlag(false);
+        }
+    }
+}
